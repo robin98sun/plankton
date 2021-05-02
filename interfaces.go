45160b32ff3223ffc9567920b7de0cf9750c7785
Robin <lxs5171@mavs.uta.edu>
2021-05-02 10:06:09 +0800

    version: 1.12.102.43.24.6.20-no_logs_and_gc

--- interfaces.go
+++ interfaces.go
@@ -4,11 +4,14 @@ import (
 	"aces/plankton/digest"
 	"aces/plankton/eat"
 	"uta.edu/aces/jadesdk"
+	"runtime/debug"
 )
 
 func main() {
 	jade := jadesdk.NewJadeSDK()
 	jade.Verbose(false)
+	// disable GC at runtime
+	debug.SetGCPercent(-1)
 
 	worker := eat.NewMouth()
 	jade.SetDefaultWorkerModule(worker)
